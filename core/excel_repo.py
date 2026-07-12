87f1acebe47ee40f67e3beedc2906e192e75f406
Yahya Hamad <yahyahamad79@gmail.com>
2026-07-12 11:46:51 +0300

    1

--- core/excel_repo.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-class ExcelRepository:
-    def __init__(self, config_manager): self.config_manager = config_manager
-    def get_resolved_path(self): return self.config_manager.excel_file
-    def load_students_df(self):
-        try: return pd.read_excel(self.get_resolved_path(), sheet_name='Students', converters={'student_id': str})
-        except: return pd.DataFrame()
--- modules/dashboard/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# تعريف الـ Blueprint الخاص بالشاشة الرئيسية
-dashboard_bp = Blueprint('dashboard', __name__)
-
-@dashboard_bp.route('/')
-def index():
-    # استدعاء ملف الـ HTML المستقل الخاص بلوحة التحكم
-    return render_template('dashboard.html')
